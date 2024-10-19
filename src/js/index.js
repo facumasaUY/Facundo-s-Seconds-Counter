949b200242c2dd7bcf84fb9c71556b82c635d27b
facumasaUY <facundomasa@gmail.com>
2024-10-19 22:43:12 +0000

    Without backwards

--- src/js/index.js
+++ src/js/index.js
@@ -7,20 +7,36 @@ import "../styles/index.css";
 
 //import your own components
 import SecondsCounter from "./component/secondsCounter.jsx";
+import { render } from "react-dom";
 
 let seconds = 0
+let interval;
+  
 
 //render your react application
 let app = ReactDOM.createRoot(document.getElementById('app'))
 app.render(<SecondsCounter counter={seconds} />);
 
-<>
-<SecondsCounter></SecondsCounter>
-</>
-
-
-setInterval(() => {
+const renderCounter = () => {
     seconds++;
-    app.render(
-    <SecondsCounter counter={seconds}/>);
-} ,1000);
+    app.render(<>
+        <SecondsCounter counter={seconds} />
+        <div>
+            <div className="btn-group" role="group" arial-label="Basic example">
+                <button onClick={()=>{
+                    clearInterval(interval)
+                    interval = setInterval(renderCounter, 1000);
+                    seconds = -1;
+                }}type="button" className="btn btn-outline-danger">Restart</button>
+                <button onClick={()=>{
+                    clearInterval(interval);
+                }} type="button" className="btn btn-outline-warning">Pause</button>
+                <button onClick={()=>{
+                    clearInterval(interval)
+                    interval = setInterval(renderCounter, 1000);
+                }} type="button" className="btn btn-outline-info">Play</button>
+            </div>
+        </div>
+    </>);
+}
+interval = setInterval (renderCounter,1000);
